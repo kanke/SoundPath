ed2e4d1100523c3a407b49dc85ec2e0aafd84bab
Bryan Yap <bryan.yap@alliants.com>
2014-12-14 08:08:25 +0000

    updated marker icon, get data from Parse

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@
   'use strict';
 
   function initialize() {
-    Parse.initialize("4RrzyjoWGy7UBIRvvurcSjAvHjb1oPNWNUUuJsBt", "b6WEyaFWuZwOt0yG0a1mmwchjQ6WmrHb8sMStp9p");
-
 /**************USE ME BYRAN************
     var Music = Parse.Object.extend("Music");
     var query = new Parse.Query(Music);
@@ -23,14 +21,16 @@
 MORE----->> https://parse.com/docs/js_guide#objects
 /***************************************/
     var stylesArray = [{"featureType":"water","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]},{"featureType":"landscape","stylers":[{"color":"#f2e5d4"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"administrative","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"road"},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{},{"featureType":"road","stylers":[{"lightness":20}]}];
-    var myLatlng = new google.maps.LatLng(51.5033630, -0.1276250);
-    var mapOptions = {
-      center: { lat: myLatlng.lat(), lng: myLatlng.lng() },
-      zoom: 13,
-      styles: stylesArray,
-      disableDefaultUI: true,
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
+
+    var currentLocation = soundpath.Geolocation.currentLocation(),
+        myLatlng = new google.maps.LatLng(currentLocation.coords.latitude, currentLocation.coords.longitude),
+        mapOptions = {
+          center: { lat: myLatlng.lat(), lng: myLatlng.lng() },
+          zoom: 15,
+          styles: stylesArray,
+          disableDefaultUI: true,
+        },
+        map = new google.maps.Map(document.getElementById('map-canvas'),
                                   mapOptions);
 
 
@@ -42,8 +42,12 @@ MORE----->> https://parse.com/docs/js_guide#objects
     var markers = [];
 
 
+    soundpath.Geolocation.on('position:changed', function (position) {
+      marker.setPosition(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+    });
+
     function circleMarker() {
-      marker.setPosition(new google.maps.LatLng(myLatlng.lat() + 0.05*(- 0.5 + Math.random()), myLatlng.lng() + 0.05*(- 0.5 + Math.random())));
+      marker.setPosition(new google.maps.LatLng(myLatlng.lat() + 0.01*(- 0.5 + Math.random()), myLatlng.lng() + 0.01*(- 0.5 + Math.random())));
       setTimeout(circleMarker, 1500);
     }
     circleMarker();
@@ -66,7 +70,7 @@ MORE----->> https://parse.com/docs/js_guide#objects
         position: new google.maps.LatLng(pos.lat(), pos.lng()),
         map: map,
         title: track.title,
-        icon: 'assets/marker-icon-1.png',
+        icon: 'assets/marker-icon-5.png',
       });
     });
   }
--- src/service.js
+++ src/service.js
@@ -1,9 +1,71 @@
 window.soundpath = {};
 
-window.soundpath.Service = (function (Q) {
+window.soundpath.Geolocation = (function (Q) {
   'use strict';
 
   var exports = {},
+      listeners = {},
+      cachedPosition = {
+        coords: {
+          latitude: 51.528,
+          longitude: -0.081,
+        }
+      };
+
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(function (position) {
+      cachedPosition = position;
+      console.log('UPDATE POSITION', position);
+
+      var array = listeners['position:changed'] || [];
+      for (var i = 0; i < array.length; i++) {
+        array[i](cachedPosition);
+      }
+    }, function (error) {
+      console.error('An error has occurred:', error);
+      switch (error.code) {
+        case error.PERMISSION_DENIED:
+          alert('Permission has been denied');
+          break;
+
+        case error.POSITION_UNAVAILABLE:
+          alert('Location information is unavailable');
+          break;
+
+        case error.TIMEOUT:
+          alert('The request to get the current location timed out.');
+          break;
+
+        case error.UNKNOWN_ERROR:
+          alert('An unknown error has occurred.');
+          break;
+      }
+    });
+  } else {
+    alert('Geolocation is not supported');
+  }
+
+
+  exports.currentLocation = function () {
+    return cachedPosition;
+  };
+
+
+  exports.on = function (key, listener) {
+    listeners[key] = listeners[key] || [];
+    listeners[key].push(listener);
+  };
+
+
+  return exports;
+}).call(this, Q);
+
+
+window.soundpath.Service = (function (Q, SC, Parse) {
+  'use strict';
+
+  var exports = {},
+      initDeferred = Q.defer(),
       listeners = {},
       cachedTracks = [],
       CLIENT_ID = 'cc6b70490609b09c4435861aff11fc6c';
@@ -14,19 +76,36 @@ window.soundpath.Service = (function (Q) {
   });
 
 
+  Parse.initialize("4RrzyjoWGy7UBIRvvurcSjAvHjb1oPNWNUUuJsBt", "b6WEyaFWuZwOt0yG0a1mmwchjQ6WmrHb8sMStp9p");
+  var Music = Parse.Object.extend("Music");
+  var query = new Parse.Query(Music);
+
+  query.find({
+    success: function(tracks) {
+      for (var i = 0; i < tracks.length; i++) {
+        var track = tracks[i];
+        cachedTracks.push({
+          title: track.attributes.song_title,
+          genre: track.attributes.stream_url,
+          artist: track.attributes.artist,
+          stream_url: track.attributes.stream_url,
+        });
+      }
+
+      initDeferred.resolve();
+    }
+  });
+
+
+  exports.ready = function () {
+    return initDeferred.promise;
+  };
+
+
   exports.seedTracks = function (options) {
     var deferred = Q.defer();
 
     SC.get('/tracks', options, function (tracks) {
-      var stats = {};
-      var i = tracks.length;
-      while (i--) {
-        var track = tracks[i];
-        stats[track.genre] = stats[track.genre] || 0;
-        stats[track.genre]++;
-      }
-      console.log(1, tracks);
-      console.log(2, stats);
       Array.prototype.push.apply(cachedTracks, tracks);
       deferred.resolve();
     });
@@ -62,4 +141,4 @@ window.soundpath.Service = (function (Q) {
 
 
   return exports;
-}).call(this, Q);
+}).call(this, Q, SC, Parse);
