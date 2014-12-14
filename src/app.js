7720b15b5a014de8099acc3d033f077e0e370406
kanke <kanke.ishaku@live.com>
2014-12-14 07:46:01 +0000

    temp

---
+++ src/app.js
@@ -0,0 +1,75 @@
+(function ($, document) {
+  'use strict';
+
+  function initialize() {
+    Parse.initialize("4RrzyjoWGy7UBIRvvurcSjAvHjb1oPNWNUUuJsBt", "b6WEyaFWuZwOt0yG0a1mmwchjQ6WmrHb8sMStp9p");
+
+/**************USE ME BYRAN************
+    var Music = Parse.Object.extend("Music");
+    var query = new Parse.Query(Music);
+    query.get("u85iVF1ZHg", {
+    success: function(music) {
+      window.alert(music.get("genre"));
+
+      // The object was retrieved successfully.
+  },
+  error: function(object, error) {
+    alert("Error: " + error.code + " " + error.message);
+    // The object was not retrieved successfully.
+    // error is a Parse.Error with an error code and message.
+  }
+});
+
+MORE----->> https://parse.com/docs/js_guide#objects
+/***************************************/
+
+    var stylesArray = [{"featureType":"water","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]},{"featureType":"landscape","stylers":[{"color":"#f2e5d4"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"administrative","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"road"},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{},{"featureType":"road","stylers":[{"lightness":20}]}];
+    var myLatlng = new google.maps.LatLng(51.5033630, -0.1276250);
+    var mapOptions = {
+      center: { lat: myLatlng.lat(), lng: myLatlng.lng() },
+      zoom: 13,
+      styles: stylesArray,
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+                                  mapOptions);
+
+
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!',
+      // icon: 'http://img3.wikia.nocookie.net/__cb20090314214419/ladygaga/images/2/28/Song_Icon.png',
+    });
+    var markers = [];
+
+
+    function circleMarker() {
+      marker.setPosition(new google.maps.LatLng(myLatlng.lat() + 0.05*(- 0.5 + Math.random()), myLatlng.lng() + 0.05*(- 0.5 + Math.random())));
+      setTimeout(circleMarker, 1500);
+    }
+    circleMarker();
+
+
+    document.addEventListener('trackchange', function (event) {
+      if (!event.detail.skipped) {
+        var pos = marker.getPosition();
+        soundpath.Service.dropTrack(event.detail.previousTrack, {
+          latitude: pos.lat(),
+          longitude: pos.lng(),
+        });
+      }
+    });
+
+
+    soundpath.Service.on('track:dropped', function (track, option) {
+      var pos = marker.getPosition();
+      new google.maps.Marker({
+        position: new google.maps.LatLng(pos.lat(), pos.lng()),
+        map: map,
+        title: track.title,
+      });
+    });
+  }
+
+  google.maps.event.addDomListener(window, 'polymer-ready', initialize);
+}).call(this, jQuery, document);
---
+++ src/service.js
@@ -0,0 +1,61 @@
+window.soundpath = {};
+
+window.soundpath.Service = (function (Q) {
+  'use strict';
+
+  var exports = {},
+      listeners = {},
+      cachedTracks = [],
+      CLIENT_ID = 'cc6b70490609b09c4435861aff11fc6c';
+
+  SC.initialize({
+      client_id: CLIENT_ID,
+      redirect_uri: 'http://localhost:3000'
+  });
+
+
+  exports.seedTracks = function (options) {
+    var deferred = Q.defer();
+
+    SC.get('/tracks', options, function (tracks) {
+      var stats = {};
+      var i = tracks.length;
+      while (i--) {
+        var track = tracks[i];
+        stats[track.genre] = stats[track.genre] || 0;
+        stats[track.genre]++;
+      }
+      console.log(1, tracks);
+      console.log(2, stats);
+      Array.prototype.push.apply(cachedTracks, tracks);
+      deferred.resolve();
+    });
+
+    return deferred.promise;
+  };
+
+
+  exports.on = function (key, callback) {
+    listeners[key] = listeners[key] || [];
+    listeners[key].push(callback);
+  };
+
+
+  exports.dropTrack = function (track, options) {
+    console.log(track.title, 'DROPPED AT', options.latitude, options.longitude);
+
+    var list = listeners['track:dropped'] || [];
+
+    for (var i = 0; i < list.length; i++) {
+      list[i](track, options);
+    }
+  };
+
+
+  exports.nextTrack = function () {
+    return cachedTracks.shift();
+  };
+
+
+  return exports;
+}).call(this, Q);
